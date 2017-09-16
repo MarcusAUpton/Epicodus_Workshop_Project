e9546c6a1d86ca254e283fb399c7d832775f0f05
Marcus_Boris <MarcusAUpton@gmail.com>
2017-09-16 14:16:07 -0700

    Make enter button activate button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  //Make pressing enter activate the button
+  $(document).bind('keypress', function(e){
+    if(e.keyCode==13){
+      $('#goal-button').trigger('click');
+    }
+  })
+
   $("#goal-button").click(function(){
     $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
     $("#new-goal").val("");
